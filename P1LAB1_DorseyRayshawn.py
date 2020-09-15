d21e2dd2125e0d6d8101c66644076fa55488585f
Shawnq261 <71301813+Shawnq261@users.noreply.github.com>
2020-09-14 22:09:04 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_DorseyRayshawn.py
@@ -0,0 +1,10 @@
+#Rayshawn Dorsey
+#Mrs. Seidi
+#Training:Basics
+#14 September 2020
+
+''' Type your code here. '''
+userNum = int(input())
+userNumSquared = userNum * userNum   
+   
+print(userNumSquared)      

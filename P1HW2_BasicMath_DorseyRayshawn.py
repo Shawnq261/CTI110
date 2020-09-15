c915a02685a61880b35683c66757bf5e019ef22c
Shawnq261 <71301813+Shawnq261@users.noreply.github.com>
2020-09-14 23:10:35 -0400

    Add files via upload
    First Submission

---
+++ P1HW2_BasicMath_DorseyRayshawn.py
@@ -0,0 +1,28 @@
+#Rayshawn Dorsey
+#Mrs. Seidi
+#BASIC MATH
+#14 September 2020
+
+''' Type your code here. '''
+Kas_num1 = int(input('Please Enter first number:')) #Type in number 2 from your keypad after display#
+firstNumber = 2
+
+Kos_num2 = int(input('Please Enter second number:')) #Type in number 2 from your keypad after display#
+secondNumber = 2
+
+print('first number entered:' , firstNumber) #Line 7 is displaying the number that was entered from line 2#
+
+print('second number entered:' , secondNumber) #Line 9 is displaying the number that was entered from line 5#
+
+print('\n') #this is a new line to separate the code above
+
+firstNumber = 2
+secondNumber = 2
+
+sum = firstNumber + secondNumber #this is an addition problem using the first and second numbers#
+sum = firstNumber * secondNumber #this is an multiplication problem using the first and second numbers#
+
+print('Sum of both numbers:' , sum, '') #the sum of the addition problem should display with an output of 4#
+
+print('Result of Multiplying both numbers:' , sum, '') # the output of the sum shoudld display here with an output of 4#
+

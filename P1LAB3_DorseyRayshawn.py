d9f39d7e959041d4124c1a3711b5a397a9a70e78
Shawnq261 <71301813+Shawnq261@users.noreply.github.com>
2020-09-14 22:10:03 -0400

    Add files via upload
    First Submissions

---
+++ P1LAB3_DorseyRayshawn.py
@@ -0,0 +1,7 @@
+#Rayshawn Dorsey
+#Mrs. Seidi
+#Hello World!
+#14 September 2020
+
+''' Type your code here. '''
+print ('Hello World!')
---
+++ P1LAB4_Parking_DorseyRayshawn.py
@@ -0,0 +1,10 @@
+#Rayshawn Dorsey
+#Mrs. Seidi
+#No Parking Sign
+#14 September 2020
+
+''' Type your code here. '''
+print('  NO PARKING')
+print('2:00' ,end=' -') #semicolon separates 2 and 00
+print(' 6:00 a.m.') #semicolon separates multiple items
+
